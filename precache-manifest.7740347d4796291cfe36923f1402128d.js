bca83bbb5f75420dc61b5eca78e55c8828ae6c7d
Kyle Wagner <Keale2@gmail.com>
2019-01-31 19:01:32 -0500

    Updates

---
+++ precache-manifest.7740347d4796291cfe36923f1402128d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6a9fd4b21733a5a7c7dc",
+    "url": "/saraandkyle.com/static/css/main.4948a317.chunk.css"
+  },
+  {
+    "revision": "6a9fd4b21733a5a7c7dc",
+    "url": "/saraandkyle.com/static/js/main.6a9fd4b2.chunk.js"
+  },
+  {
+    "revision": "095429da2d149ee0c099",
+    "url": "/saraandkyle.com/static/js/1.095429da.chunk.js"
+  },
+  {
+    "revision": "4edb8af8762afe2e85f5",
+    "url": "/saraandkyle.com/static/js/runtime~main.4edb8af8.js"
+  },
+  {
+    "revision": "70c7c2164043ed0030f0f115daba0334",
+    "url": "/saraandkyle.com/index.html"
+  }
+];
